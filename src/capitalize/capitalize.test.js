6f0473e59a836f3a119c1c50c639ac80ddf99c88
M.Aldi Gunawan <aldigunawan1808@gmail.com>
2024-10-06 11:49:04 +0700

    Add capitalize.js and its corresponding tests
    - Created capitalize.js with a function that capitalizes the first letter of a given string.
    - Added capitalize.test.js with unit tests to ensure the function behaves as expected.
      - Test cases include normal strings
      - single characters
      - empty string
      - strings that begin with non-alphabetical characters.
    - Confirmed the tests pass, following the TDD process.

---
+++ src/capitalize/capitalize.test.js
@@ -0,0 +1,21 @@
+import capitalize from "./capitalize";
+
+test('Capitalize one letter', () => {
+  expect(capitalize('h')).toBe('H')
+})
+
+test('Capitalize one word', () => {
+  expect(capitalize('hello')).toBe('Hello')
+})
+
+test('Capitalize', () => {
+  expect(capitalize('hello world')).toBe('Hello world')
+})
+
+test('Not Capitalize if theres no character at all', () => {
+  expect(capitalize('')).toBe('')
+})
+
+test('Not Capitalize if first character are not letter', () => {
+  expect(capitalize('3, 2, 1, hello')).toBe('3, 2, 1, hello')
+})
